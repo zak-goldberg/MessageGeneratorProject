544b8f0f262f2560aac96a8d7c2fd7ad631b6c3c
Zak Goldberg <zakgoldberg@HMB69.local>
2024-06-19 08:40:56 -0700

    Initial message generator script commit.

---
+++ messagegenerator.js
@@ -0,0 +1,29 @@
+// Create arrays of Spanish phrases
+const messageArray1 = ['¡Hola!', 'Buenas.', 'Buenas noches.', '¡Bienvenido!', 'Que te vaya bien!'];
+const messageArray2 = ['¿Qué te gusta hacer?', '¡Me gustan las albondigas!', '¡Me encanta el basquetbol!', 'No me interesa las noticias de celebridades.', 'Me gusta ir al museo \"the Legion of Honor\" en San Fransisco.'];
+const messageArray3 = ['¡Feliz cumpleaños!', '¡Buena suerte!', '¡Bien hecho!', '¡Cuídate!', '¡Felicitaciones!', '¡Salud!'];
+
+// Package the arrays into an object
+const messageObj = {
+    array1: messageArray1,
+    array2: messageArray2,
+    array3: messageArray3
+}
+
+// Function to concatenate one random phrase from each array
+function returnRandomMessage(packagedArrays) {
+  // Create array of arrays
+  let arrayValues = Object.values(packagedArrays);
+  // Create an array of random values from the arrays
+  let randomValues = arrayValues.map((subArray) => {
+    let randomIndex = Math.floor(Math.random() * subArray.length);
+    return subArray[randomIndex];
+  });
+  // Concatenate the random values and return them
+  return randomValues.join(' ');
+}
+
+// Print 3 random concantenated strings to the console
+for (let i = 0; i < 3; i++) {
+    console.log(returnRandomMessage(messageObj));
+}
